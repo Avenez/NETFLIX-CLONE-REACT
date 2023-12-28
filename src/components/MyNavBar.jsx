a9288ce6cec2d193fff71634c801ae4c068a7865
Avenez <145536778+Avenez@users.noreply.github.com>
2023-12-28 16:45:56 +0100

    navMod

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,68 +1,86 @@
-import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import React, { Component } from "react";
+import { Navbar, Nav, Button, Form, Col } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import InputGroup from "react-bootstrap/InputGroup";
+import MyProfileSelect from "./MyProfileSalect";
 
-const MyNavBar = () => {
-  return (
-    <Navbar expand="lg" bg="dark">
-      <div className="container-fluid">
-        <a target="_blank" className="navbar-brand" href="/">
-          <img
-            className="ps-"
-            height="50"
-            src="https://ongpng.com/wp-content/uploads/2023/04/7.Netflix-Logo-1728x734-1.png"
-            alt="netflixlogo"
-          />
-        </a>
-        <Navbar.Toggle aria-controls="navbarSupportedContent" className="btn btn-dark" />
-        <Navbar.Collapse id="navbarSupportedContent">
-          <Nav className="me-auto mb-2 mb-lg-0">
-            <Nav.Item>
-              <Nav.Link target="_blank" className="nav-link active text-white" href="#">
-                Home
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link target="_blank" className="nav-link text-white fw fw-bold" href="#">
-                TV Show
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link target="_blank" className="nav-link active text-white" href="#">
-                Movies
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link target="_blank" className="nav-link active text-white" href="#">
-                Recently Added
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link target="_blank" className="nav-link active text-white" href="#">
-                My List
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <div className="d-flex justify-content-end justify-content-lg-between align-items-center" role="search">
-            <Button className="btn btn-dark p-0 mx-4" type="button">
-              <i className="bi bi-search"></i>
-            </Button>
-            <a className="text-white m-0 text-decoration-none pointer-event">KIDS</a>
-            <Button className="btn btn-dark p-0 ms-4 me-2" type="button">
-              <i className="bi bi-bell-fill"></i>
-            </Button>
-            <div className="btn ms-0">
-              <a href="http://127.0.0.1:5500/profile.html" target="_blank">
-                <img src="assets/avatar.png" width="30" alt="avatar" />
+class MyNavBar extends Component {
+  state = {
+    searchIsActive: false,
+  };
+
+  render() {
+    return (
+      <Navbar expand="lg" bg="dark">
+        <div className="container-fluid">
+          <a target="_blank" className="navbar-brand" href="/">
+            <Image
+              className="ps-"
+              height="50"
+              src="https://ongpng.com/wp-content/uploads/2023/04/7.Netflix-Logo-1728x734-1.png"
+              alt="netflixlogo"
+            />
+          </a>
+          <Navbar.Toggle aria-controls="navbarSupportedContent" className="btn btn-dark" />
+          <Navbar.Collapse id="navbarSupportedContent">
+            <Nav className="me-auto mb-2 mb-lg-0">
+              <Nav.Item>
+                <Nav.Link target="_blank" className="nav-link" href="#">
+                  Home
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link target="_blank" className="nav-link" href="#">
+                  TV Show
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link target="_blank" className="nav-link" href="#">
+                  Movies
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link target="_blank" className="nav-link" href="#">
+                  Recently Added
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link target="_blank" className="nav-link" href="#">
+                  My List
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+
+            {this.state.searchIsActive && (
+              <div className="d-flex justify-content-center align-items-center mb-0 ">
+                <InputGroup className="mb-3 searcForm d-flex justify-content-center align-items-center pt-3 mb-0 pb-0">
+                  <Form.Control placeholder="search..." aria-label="Username" size="sm" className="rounded-pill" />
+                </InputGroup>
+              </div>
+            )}
+            <div className="d-flex justify-content-end  align-items-center flex-wrap" role="search">
+              <a
+                className="search p-0 mx-4 nav-link mt-3"
+                type="button"
+                onClick={() => {
+                  this.setState({ searchIsActive: this.state.searchIsActive ? false : true });
+                }}
+              >
+                <i className="bi bi-search"></i>
               </a>
-              <Button className="btn btn-dark p-0" type="button">
-                <i className="bi bi-caret-down-fill"></i>
+              <a className="nav-link m-0 text-decoration-none pointer-event">KIDS</a>
+              <Button className="btn btn-dark p-0 ms-4 me-2" type="button">
+                <i className="bi bi-bell-fill"></i>
               </Button>
+              <div className="btn ms-0">
+                <MyProfileSelect />
+              </div>
             </div>
-          </div>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
-  );
-};
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    );
+  }
+}
 
 export default MyNavBar;
---
+++ src/components/MyProfileSalect.jsx
@@ -0,0 +1,32 @@
+import { Component } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
+
+class MyProfileSelect extends Component {
+  render() {
+    return (
+      <DropdownButton
+        variant="dark"
+        id="dropdown-basic-button"
+        title={<Image src="assets/avatar.png" width="30" alt="avatar" rounded />}
+        drop="start"
+        data-bs-theme="dark"
+      >
+        <Dropdown.Item href="#/action-1">
+          <i className="bi bi-people-fill me-2 "></i>Cambia Profilo
+        </Dropdown.Item>
+        <Dropdown.Item href="#/action-2">
+          <i className="bi bi-person-plus me-2"></i>Aggiungi Profilo
+        </Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item href="#/action-3">
+          <i className="bi bi-gear-wide me-2"></i>Impostazioni
+        </Dropdown.Item>
+      </DropdownButton>
+    );
+  }
+}
+
+export default MyProfileSelect;
